b90906e64e0d898f13e35d8e7230c5d9b5e10f84
Norby141 <100698239+Norby14@users.noreply.github.com>
2022-03-12 11:45:15 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Liczby parzyste:");
+		int i;
+		for (i=0 ; i<101 ;i++)
+			{
+				if (i % 2==0)
+				System.out.print(i + ", ");
+			}
+  }
+}
